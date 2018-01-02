038269bd39d3f3514ab46b9d308a26a1d4d71cb2
Kevin-Lo <luokaiwen168@gmail.com>
2018-01-02 15:14:43 -0800

    dropdown

--- navigationbar.js
+++ navigationbar.js
@@ -11,3 +11,17 @@ function closeNav() {
     document.getElementById("sidebar").style.transition = "1.5s";
     document.getElementById("sidebar").style.width = "50px";
 }
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+    var myDropdown = document.getElementById("myDropdown");
+      if (myDropdown.classList.contains('show')) {
+        myDropdown.classList.remove('show');
+      }
+  }
+}
